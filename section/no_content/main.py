fc08a198d8a8a830d867ab83fc4e45f632169f70
Fanabeam <>
2026-05-23 07:56:04 +0000

    No content

---
+++ section/no_content/main.py
@@ -0,0 +1,14 @@
+# Number of classes and students per class
+classes = 6
+students = 27
+
+# Calculate total number of students
+total_students = students * classes
+
+# Calculate total number of classes
+students = 27
+total_classes = total_students/students
+
+# Testing
+print("Number of students:", total_students)
+print("Number of groups:", total_classes)
